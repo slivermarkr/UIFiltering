41a5ab563eb0bb9be49d95fa34dd73b5bafcd02f
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-05-29 19:21:02 +0800

    redo

--- main.js
+++ main.js
@@ -1,158 +1,157 @@
 const products = [
  {
    name: 'Sony Playstation 5',
-   url: 'https://i.ibb.co/zHmZnWx/playstation-5.png',
+   url: 'images/playstation_5.png',
    category: 'games',
    price: 499.99,
  },
  {
    name: 'Samsung Galaxy',
-   url: 'https://i.ibb.co/rFFMDH7/samsung-galaxy.png',
+   url: 'images/samsung_galaxy.png',
    category: 'smartphones',
    price: 399.99,
  },
  {
    name: 'Cannon EOS Camera',
-   url: 'https://i.ibb.co/mhm1hLq/cannon-eos-camera.png',
+   url: 'images/cannon_eos_camera.png',
    category: 'cameras',
    price: 749.99,
  },
  {
    name: 'Sony A7 Camera',
-   url: 'https://i.ibb.co/LS9TDLN/sony-a7-camera.png',
+   url: 'images/sony_a7_camera.png',
    category: 'cameras',
    price: 1999.99,
  },
  {
    name: 'LG TV',
-   url: 'https://i.ibb.co/QHgFnHk/lg-tv.png',
+   url: 'images/lg_tv.png',
    category: 'televisions',
    price: 799.99,
  },
  {
    name: 'Nintendo Switch',
-   url: 'https://i.ibb.co/L0L9SdG/nintendo-switch.png',
+   url: 'images/nintendo_switch.png',
    category: 'games',
    price: 299.99,
  },
  {
    name: 'Xbox Series X',
-   url: 'https://i.ibb.co/C8rBVdT/xbox-series-x.png',
+   url: 'images/xbox_series_x.png',
    category: 'games',
    price: 499.99,
  },
  {
    name: 'Samsung TV',
-   url: 'https://i.ibb.co/Pj1nm4B/samsung-tv.png',
+   url: 'images/samsung_tv.png',
    category: 'televisions',
    price: 1099.99,
  },
  {
    name: 'Google Pixel',
-   url: 'https://i.ibb.co/5F58zmH/google-pixel.png',
+   url: 'images/google_pixel.png',
    category: 'smartphones',
    price: 499.99,
  },
  {
    name: 'Sony ZV1F Camera',
-   url: 'https://i.ibb.co/5Wy3RZ9/sony-zv1f-camera.png',
+   url: 'images/sony_zv1f_camera.png',
    category: 'cameras',
    price: 799.99,
  },
  {
    name: 'Toshiba TV',
-   url: 'https://i.ibb.co/FxM6rXq/toshiba-tv.png',
+   url: 'images/toshiba_tv.png',
    category: 'televisions',
    price: 499.99,
  },
  {
    name: 'iPhone 14',
-   url: 'https://i.ibb.co/5vc7J6s/iphone-14.png',
+   url: 'images/iphone_14.png',
    category: 'smartphones',
    price: 999.99,
  },
 ];
 
-const productsWrapper =  document.querySelector('#products-wrapper');
-const checkboxes = document.querySelectorAll('.checkbox');
-const countCart  = document.querySelector('#cart-count');
-const searchInput = document.querySelector('#search');
+const productWrapper = document.querySelector('#products-wrapper');
 const filterContainer = document.querySelector('#filters-container');
+const checkboxes = document.querySelectorAll('.check');
+const searchInput = document.querySelector('#search');
+const cartCountDiv = document.querySelector('#cart-count');
+
+searchInput.addEventListener('input',filterProducts);
+filterContainer.addEventListener('change',filterProducts);
 
 let cartItemCount = 0;
 
-const productElements = []
-
-filterContainer.addEventListener('change',filterProducts);
-searchInput.addEventListener('input',filterProducts);
+const productElements = [];
 
 products.forEach(product => {
- const productElement = createProductElement(product);
- productElements.push(productElement);
- productsWrapper.appendChild(productElement);
+const productElement = createProductElement(product);
+productElements.push(productElement);
+productWrapper.appendChild(productElement);
 })
 
-
 function createProductElement(product) {
- const productElement = document.createElement('div');
- productElement.className = "item space-y-2";
+  const div = document.createElement('div');
+  div.className = `item space-y-2`;
 
- productElement.innerHTML = `
- <div
- class="bg-gray-100 flex justify-center relative overflow-hidden group cursor-pointer border rounded-xl"
->
- <img
-   src="${product.url}"
-   alt="${product.name}"
-   class="w-full h-full object-cover"
- />
- <span
-   class="status bg-black text-white absolute bottom-0 left-0 right-0 text-center py-2 translate-y-full transition group-hover:translate-y-0"
-   >Add To Cart</span
- >
-</div>
+  div.innerHTML = `
+  <div 
+    class="bg-gray-100 flex-justify-center relative      overflow-hidden group cursor-pointer border rounded-xl">    
+    <img 
+    src="${product.url}" 
+    alt="${product.name}" 
+    class="w-full-h-full object-cover">    
+    </img>
+    <span
+    class="status bg-black text-white absolute bottom-0 left-0 right-0 text-center py-2 translate-y-full transition group-hover:translate-y-0"
+    >Add To Cart</span>
+  </div>
 <p class="text-xl">${product.name}</p>
 <strong>$${product.price.toLocaleString()}</strong>
- `
- productElement.querySelector(".status").addEventListener('click',updateCart);
- return productElement
+  `
+  div.querySelector('.status').addEventListener('click',updateCart);
+
+  return div
 }
 
 function updateCart(e) {
- const statusEl = e.target
- if(statusEl.classList.contains('added')) {
-  statusEl.classList.remove('added');
-  statusEl.textContent = "Add To Cart";
-  statusEl.classList.add('bg-gray-800');
-  statusEl.classList.remove('bg-red-600');
-  
-  cartItemCount--;
- } else {
-  statusEl.classList.add('added');
-  statusEl.textContent = "Remove from Cart";
-  statusEl.classList.remove('bg-gray-800');
-  statusEl.classList.add('bg-red-600');
-  cartItemCount++;
- }
- countCart.innerText = cartItemCount.toString();
+  const statusEl = e.target
+  if(statusEl.classList.contains('added')) {
+    statusEl.classList.remove('added');
+    statusEl.textContent = "Add To Cart"
+    statusEl.classList.add('bg-gray-800');
+    statusEl.classList.remove('bg-red-600');
+
+    cartItemCount--
+  }else {
+    statusEl.classList.add('added');
+    statusEl.textContent = "Remove From Cart"
+    statusEl.classList.remove('bg-gray-800');
+    statusEl.classList.add('bg-red-600');
+
+    cartItemCount++
+  }
+  cartCountDiv.innerHTML = cartItemCount.toString();
 }
 
 function filterProducts() {
- const searchTerm = searchInput.value.trim().toLowerCase();
- const checkedCategories = Array.from(checkboxes)
-.filter((check)=>check.checked)
-.map(check=>check.id);
+  const searchTerm = searchInput.value.trim().toLowerCase();
+  const checkedCategories = Array.from(checkboxes)
+  .filter((check) => check.checked)
+  .map(check => check.id);
 
- productElements.forEach((productElement,index) => {
-  const product = products[index];
+  productElements.forEach((productElement,index) => {
+    const product = products[index];
 
-  const matchesSearchTerm = product.name.toLowerCase().includes(searchTerm);
-  const isInCheckedCategory = checkedCategories.length === 0 || checkedCategories.includes(product.category);
+    const matchesSearchTerm = product.name.toLowerCase().includes(searchTerm);
+    const isIncheckedCategory = checkedCategories.length === 0 || checkedCategories.includes(product.category);
 
-  if(matchesSearchTerm && isInCheckedCategory) {
-   productElement.classList.remove('hidden');
-  } else {
-   productElement.classList.add('hidden');
-  }
- })
+    if(matchesSearchTerm && isIncheckedCategory) {
+      productElement.classList.remove('hidden');
+    } else {
+      productElement.classList.add('hidden');
+    }
+  })
 }
--- script2.js
+++ script2.js
@@ -1,9 +1,99 @@
 
 
-for(let i = 0; i < 8; i++) {
- let symbol = ""
- for(j = 0; j < i; j++) {
-  symbol += '#'
- }
- console.log(symbol);
+// for(let num = 1; num <= 100; num++) {
+//  if(num % 5 === 0 && num % 3 === 0) {
+//   console.log("FizzBuzz");
+//  }
+//   else if(num % 3 === 0 || num % 5 === 0) {
+//   if(num % 3 === 0) {
+//    console.log("Buzz");
+//   }
+//   if(num % 5 ===0) {
+//    console.log("Fizz")
+//   }
+//  }else {
+//   console.log(num);
+//  }
+ 
+// }
+
+// for(let num = 1; num <= 100; num++) {
+//  if(num % 3 === 0 && num % 5 === 0) {
+//   console.log("FizzBuzz");
+//  } else if (num % 3 === 0 || num % 5 === 0) {
+//   if(num % 3 === 0) {
+//    console.log("Buzz");
+//   }
+//   if(num % 5 === 0) {
+//    console.log("Fizz");
+//   }
+//  } else {console.log(num)};
+// }
+
+// function chessGrid(n = 8) {
+//  for(let i = 0; i < n; i++) {
+//   let output = ""
+//   if(i % 2 === 0) output += "#";
+//   for(let j = 0; j < n; j++) {
+//    if(output.length % 2 === 0) {
+//     output += " "
+//    } else {
+//     output += "#";
+//    }
+//   }
+//   console.log('\n');
+//  }
+// }
+
+// function chessGrid(n) {
+//  for(let i = 0; i < n; i++) {
+//   let output = ""
+//   for(let j = 0; j < n; j++) {
+//    if((j + i) % 2 === 0) {
+//     output += " "
+//    } else {
+//     output += "#";
+//    }
+//   }
+//   console.log(output)
+//   console.log('\n');
+  
+//  }
+// }
+// chessGrid(8)
+// i
+
+// let size = 8;
+
+// const hummus = (factor) => {
+//  const ingredient = (amount,unit,name) => {
+//   let ingredientAmount = amount * factor;
+//   if(ingredientAmount > 1) {
+//    unit += "s";
+//  };
+//  console.log(`${ingredientAmount} ${unit} ${name}`)
+//  }
+//  ingredient(1, "can", "chickpeas");
+//  ingredient(0.25, "cup", "tahini");
+//  ingredient(0.25, "cup", "lemon juice");
+//  ingredient(1, "clove", "garlic");
+//  ingredient(2, "tablespoon", "olive oil");
+//  ingredient(0.5, "teaspoon", "cumin");
+// }
+
+// hummus(3);
+
+function Cell(n) {
+ const value = n
+
+ const getValue = () => value
+
+ return () => value
 }
+
+const one = Cell(1);
+const two = Cell(2);
+
+
+console.log(one())
+console.log(two());
